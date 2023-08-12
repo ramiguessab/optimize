f3b263279d976bc9864fe05192781ab43ea04db1
ramiguessab <ramiguessab94@gmail.com>
2023-08-12 02:38:11 +0100

    added qr scanner

--- app/admin/check-in/page.tsx
+++ app/admin/check-in/page.tsx
@@ -2,30 +2,27 @@
 import { useState, useEffect } from "react";
 import { Html5Qrcode } from "html5-qrcode";
 import { Flashlight, FlashlightOff } from "lucide-react";
-import {
-    Html5QrcodeCameraScanConfig,
-    Html5QrcodeConfigs,
-} from "html5-qrcode/esm/html5-qrcode";
 
 export default function CheckIn() {
     useEffect(() => {
         const html5QrCode = new Html5Qrcode("qr");
-        const qrCodeSuccessCallback = (decodedText: string) => {
-            console.log(decodedText);
-        };
-        const config: Html5QrcodeCameraScanConfig = {
-            fps: 30,
-            qrbox: { width: 250, height: 250 },
-            videoConstraints: { aspectRatio: 4 / 3, facingMode: "environment" },
-        };
 
-        // If you want to prefer front camera
-        html5QrCode.start({}, config, qrCodeSuccessCallback, () => {});
+        html5QrCode.start(
+            { facingMode: "environment" },
+            {
+                fps: 30,
+                qrbox: { width: 300, height: 300 },
+            },
+            (text) => {
+                console.log(text);
+            },
+            () => {}
+        );
     }, []);
 
     return (
-        <div>
-            <div id="qr"></div>
+        <div className="h-[calc(100dvh_-_106px)] flex flex-col justify-center items-center">
+            <div id="qr" />
         </div>
     );
 }
